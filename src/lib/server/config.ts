a46bbaa5d031ea265fc75169f14e2a5631dfcd08
TequinDragon <github@tequ.in>
2024-06-17 01:01:15 +1200

    🧹 House Cleaning - apply formatting

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -35,30 +35,27 @@ class Configuration {
 		this.data = {};
 	}
 
-	public async commit (): Promise<void> {
-		const payload = JSON.stringify(this.data)
+	public async commit(): Promise<void> {
+		const payload = JSON.stringify(this.data);
 		// console.debug(`Writing config to ${this.path}`, this.data, payload)
 		await fs.writeFile(this.path, JSON.stringify(this.data), 'utf-8');
 	}
 
 	public load = async (): Promise<void> => {
 		this.data = await this.read();
-		if (this.data === null)
-			this.data = {}
+		if (this.data === null) this.data = {};
 	};
 
 	private async read(): Promise<config> {
-		const payload = await fs.readFile(this.path, 'utf-8')
+		const payload = await fs.readFile(this.path, 'utf-8');
 		const data = JSON.parse(payload, (_key, value) => {
-			if (typeof(value) !== "string")
-				return value
+			if (typeof value !== 'string') return value;
 
 			const date = Date.parse(value);
-			if (!isNaN(date))
-				return new Date(date);
+			if (!isNaN(date)) return new Date(date);
 
 			return value;
-		})
+		});
 
 		// console.debug(`Loaded data from ${this.path}`, payload, data)
 		return data;
--- src/routes/overlay/events/+server.ts
+++ src/routes/overlay/events/+server.ts
@@ -2,46 +2,45 @@ import type { RequestHandler } from './$types';
 import { TwitchStuff, type TwitchEventEmitter } from '$lib/server/twitch';
 import { emitter } from '$lib/server/events';
 
-import { EventSubChannelFollowEvent } from '@twurple/eventsub-base'
+import { EventSubChannelFollowEvent } from '@twurple/eventsub-base';
 import { produce } from 'sveltekit-sse';
 
 import type { ToastMessage } from '$lib/Toast.svelte';
 
 export const POST: RequestHandler = (request) => {
 	return produce(async ({ emit, lock }) => {
-		console.log('Starting new overlay SSE session')
+		console.log('Starting new overlay SSE session');
 
 		const onChannelFollow = (event: EventSubChannelFollowEvent) => {
-			console.log(`Got a follow from ${event.userDisplayName}!`)
+			console.log(`Got a follow from ${event.userDisplayName}!`);
 			const message: ToastMessage = {
 				duration: 10_000,
 				message: `New follow from ${event.userDisplayName}!`,
-				time: Date.now()
-			}
-			emit('toast', JSON.stringify(message))
-		}
+				time: Date.now(),
+			};
+			emit('toast', JSON.stringify(message));
+		};
 		const toastListener = (message: ToastMessage) => {
 			emit('toast', JSON.stringify(message));
-		}
+		};
 
 		let eventSub: TwitchEventEmitter | null = null;
-		try{
+		try {
 			emitter.on('toast', toastListener);
 
-			eventSub = await TwitchStuff.getEventSubListener()
-			eventSub.on('onChannelFollow', onChannelFollow)
-		}catch(e) {
-			console.error("There was error with getting the EventSub")
+			eventSub = await TwitchStuff.getEventSubListener();
+			eventSub.on('onChannelFollow', onChannelFollow);
+		} catch (e) {
+			console.error('There was error with getting the EventSub');
 			// Allow the SSE to close by unlocking it
-			lock.set(false)
-		}
-		finally {
+			lock.set(false);
+		} finally {
 			return (stream) => {
-				console.log('Stopping overlay SSE session')
+				console.log('Stopping overlay SSE session');
 
-				emitter.off('toast', toastListener)
-				eventSub?.off('onChannelFollow', onChannelFollow)
-			}
+				emitter.off('toast', toastListener);
+				eventSub?.off('onChannelFollow', onChannelFollow);
+			};
 		}
 	});
 };
